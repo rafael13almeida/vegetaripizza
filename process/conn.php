0c7ee673f2d4672e936835270b7c2237937fe7a5
Rafael Almeida <rafael13almeida@gmail.com>
2022-07-22 15:45:54 -0300

    Atualizando gitignore

--- process/conn.php
+++ process/conn.php
@@ -9,6 +9,9 @@ $host = "localhost";
 try {
 
     $conn = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    
 
 } catch(PDOException $e) {
 
