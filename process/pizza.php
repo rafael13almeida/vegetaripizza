eebd31f60f99bf5573313db76fc2758a55267560
Rafael Almeida <rafael13almeida@gmail.com>
2022-07-22 20:35:59 -0300

    Inserindo os dados na home

--- process/pizza.php
+++ process/pizza.php
@@ -1,3 +1,21 @@
 <?php
 include_once("conexao.php");
-?>
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+if ($method === "GET") {
+
+    $bordasQuery = $conexao->query("SELECT * FROM bordas;");
+    $bordas = $bordasQuery->fetchAll();
+
+    $massasQuery = $conexao->query("SELECT * FROM massas;");
+    $massas = $massasQuery->fetchAll();
+
+    $saboresQuery = $conexao->query("SELECT * FROM sabores;");
+    $sabores = $saboresQuery->fetchAll();
+
+} else if ($method === "POST") {
+
+    echo "a";
+
+}
